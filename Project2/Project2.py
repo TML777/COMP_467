5735c6fb45615ac8198871358b9ef62ba1907571
TML777 <92769252+TML777@users.noreply.github.com>
2024-10-23 22:29:51 -0700

    CSV and XLSX fiels

--- Project2/Project2.py
+++ Project2/Project2.py
@@ -1,5 +1,7 @@
 import pymongo
+import pandas as pd
 import argparse
+import sys
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 db = myclient["BugReports"]
@@ -8,7 +10,33 @@ collection1 = db["Collection1"]
 collection2 = db["Collection2"]
 
 
-cursor = collection1.find({})
-for ent in cursor:
-    print(ent["Test #"])
 
+parser = argparse.ArgumentParser(description='The Reckoning parser')
+parser.add_argument('--file', type = str, required=False, 
+                    help = 'Directory to csv or xlsx to import into datablse')
+parser.add_argument('--collection', type = str,choices=["Collection1", "Collection2"],
+                     required='--file' in sys.argv, help = 'Collectoin name')
+parser.add_argument('--export_user', type = str, help = 'Export to a csv file a specific user')
+
+args = parser.parse_args()
+
+if(args.file):
+    collection = db[args.collection]
+    
+    if(".xlsx" in args.file):
+        df = pd.read_excel(args.file)
+    elif(".csv" in args.file):
+        df = pd.read_csv(args.file)
+
+    data = df.to_dict(orient='records')
+    
+    if data:
+        collection.insert_many(data)
+        print(f"Successfully inserted {len(data)} records into MongoDB")
+    else:
+        print("No data to insert.")
+    
+
+if(args.export):
+    myList = list(collection1.find({"Test #": 0}, {"_id": 0}))
+    print(myList)
